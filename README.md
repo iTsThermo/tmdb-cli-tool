ed37b77cb79ba512e033eea9f3556ae08cfd1aab
iTsThermo <117327847+iTsThermo@users.noreply.github.com>
2025-03-25 17:03:28 -0400

    Use case for args

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ To use (while in main dir): - cmake -S . -B <Path>
                             - cd build
                             - make 
 
+Dependencies:
+            -sudo apt-get install libv8-dev
+
+
 Adding to cli: export PATH=$PATH:<Path>
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,76 @@
 #include <stdio.h>
 #include <iostream>
+// #include <v8/v8-json.h>
+#include <string>
+#include <bits/stdc++.h> 
+#include <array>
+
+void print_help_manual() {
+    std::cout << "You can use the following commands to find the latest movies" << std::endl 
+    << "\"--type\" " << "followed by:" << std::endl
+    << "\"playing\"" << std::endl 
+    << "\"popular\"" << std::endl
+    << "\"top\"" << std::endl
+    << "\"upcoming\"" << std::endl;
+}
 
 int main(int argc, char *argv[])
-{
-    std::cout << "Hello World" << std::endl;
+{   
+    // Check for CLI arguements
+    // If not valid arguements prompt user with manual
+    std::cout << "Num of args: " << argc << std::endl;
+
+    if (argc != 3) {
+        print_help_manual();
+        return 1;
+    }
+
+    // std::cout << argv[0] << std::endl;
+    // std::cout << argv[1] << std::endl;
+    // std::cout << argv[2] << std::endl;
+
+    std::string type_arg_trimmed = argv[1];
+
+    std::cout << type_arg_trimmed << std::endl;
+
+    type_arg_trimmed.erase(std::remove_if(type_arg_trimmed.begin(), type_arg_trimmed.end(), ::isspace), type_arg_trimmed.end());
+
+    if (type_arg_trimmed != "--type") {
+        print_help_manual();
+        return 1;
+    }
+
+    std::string category_arg_lowercase = argv[2];
+
+    std::transform(category_arg_lowercase.begin(), category_arg_lowercase.end(), category_arg_lowercase.begin(), ::tolower); 
+    
+    category_arg_lowercase.erase(std::remove_if(category_arg_lowercase.begin(), category_arg_lowercase.end(), ::isspace), category_arg_lowercase.end());
+
+    std::string movie_categories[] = {"playing", "popular", "top", "upcoming"};
+
+    bool category_found = false;
+
+    for (int i = 0; i < sizeof(movie_categories); i++) {
+        if (movie_categories[i] == category_arg_lowercase) {
+            category_found = true;
+        }
+    }
+
+    std::cout << category_found;
+
+    if (category_found == false) {
+        print_help_manual();
+        return 1;
+    }
+
+    // std::cout << category_arg_lowercase << std::endl;
+
+    // struct {
+    //     std::string name;
+    //     int8_t age;
+    // } person;
+
+
+    // std::cout << "Hello World" << std::endl;
     return 0;
 }
